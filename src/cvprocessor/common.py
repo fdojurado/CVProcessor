7cff7d1ea60d62f9283c9de1fd4f10fff8ad2e1c
Fernando Jurado <ffjla@dtu.dk>
2024-05-13 12:16:05 +1000

    Common module added docstring

--- src/cvprocessor/common.py
+++ src/cvprocessor/common.py
@@ -1,9 +1,17 @@
+"""
+This module contains common functions that are used in the cvprocessor module.
+"""
 import pandas as pd
 
 
 def check_nan(value) -> bool:
-    if pd.isna(value):
-        is_nan = True
-    else:
-        is_nan = False
+    """
+    Check if a value is NaN.
+
+    :param value: The value to check.
+    :type value: Any
+    :return: True if the value is NaN, False otherwise.
+    :rtype: bool
+    """
+    is_nan = pd.isna(value)
     return is_nan
