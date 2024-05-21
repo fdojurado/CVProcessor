46aa5d4246bf199b98943a6a1d517c18d772d78f
Fernando Jurado <ffjla@dtu.dk>
2024-05-21 12:25:28 +1000

    Added corrdinates processing for the contact obj

--- src/cvprocessor/contact/contact.py
+++ src/cvprocessor/contact/contact.py
@@ -33,7 +33,7 @@ class Contact:
         self.location = str()
         self.city = str()
         self.country = str()
-        self.coordinates = str()
+        self.coordinates = tuple()
 
     def get_email(self):
         """
@@ -77,13 +77,40 @@ class Contact:
         """
         return self.coordinates
 
+    def convert_coordinates(self, lalng):
+        """
+        Convert the latitude and longitude coordinates to a float value.
+        """
+        lalng = lalng.replace("°", "").strip()
+        if "S" in lalng or "W" in lalng:
+            lalng = lalng.replace("S", "").replace("W", "")
+            lalng = "-" + lalng
+        else:
+            lalng = lalng.replace("N", "").replace("E", "")
+        return float(lalng)
+
+    def process_coordinates(self, coordinates):
+        """
+        Process the coordinates to convert them to a tuple of floats.
+        """
+        if coordinates is not None:
+            # split into latitude and longitude
+            coordinates = coordinates.split(", ")
+            coordinates = [self.convert_coordinates(
+                coord) for coord in coordinates]
+            self.coordinates = tuple(coordinates)
+
     def load(self, df):
         """
         Loads the contact information of the author.
         """
         for key, value in contact_types.items():
             if value in df and not pd.isnull(df[value]):
-                setattr(self, key, df[value])
+                # check if we are processing the coordinates
+                if key == "coordinates":
+                    self.process_coordinates(df[value])
+                else:
+                    setattr(self, key, df[value])
             else:
                 setattr(self, key, str())
 
